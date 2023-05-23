c26cd081dedb213874016e7846c61a93a28e412e
MrH <1062004566@qq.com>
2023-05-23 17:39:20 +0800

    aaa

--- VRvedio/app/src/main/java/com/example/vrvedio/secondActivity.java
+++ VRvedio/app/src/main/java/com/example/vrvedio/secondActivity.java
@@ -1,53 +1,68 @@
 package com.example.vrvedio;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
 
 import java.io.IOException;
 
-public class secondActivity extends AppCompatActivity {
+public class secondActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private VideoView videoView;
     private MediaPlayer mediaplayer;
     private SurfaceView sv;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        try {
-            initvideo();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        initvideo();
+
     }
 
-    private void initvideo() throws IOException {
+    private void initvideo()  {
+        //添加权限
+        ActivityCompat.requestPermissions(this,new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
         //资源地址
         String uri="android.resource://"+getPackageName()+"/"+R.raw.airg1;
         //创建SurfaceView，视频播放在SurfaceView容器内部
         sv= findViewById(R.id.view);
-        //创建MediaPlayer对象，搭配SurfaceHolder进行一系列视频播放操作
-        SurfaceHolder holder = sv.getHolder();
-        holder.addCallback((SurfaceHolder.Callback) this);
         ////设置显示视频显示在SurfaceView上
         mediaplayer=new MediaPlayer();
-        mediaplayer.setDataSource(this,Uri.parse(uri));
-        mediaplayer.setDisplay(holder);
+        try {
+            mediaplayer.setDataSource(this,Uri.parse(uri));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         ////异步方式播放视频
         mediaplayer.prepareAsync();
         mediaplayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
+                mediaplayer.setDisplay(sv.getHolder());
                 mediaplayer.start();
             }
         });
+        btn = findViewById(R.id.btn2);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(secondActivity.this,MainActivity.class);
+        startActivity(intent);//完成跳转
     }
 }
